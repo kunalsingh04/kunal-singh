36b9e533ed0b35cebee42f2d329397cf83670f63
kunalsingh04 <75198769+kunalsingh04@users.noreply.github.com>
2021-01-09 14:45:20 +0530

    Add files via upload

---
+++ natural.c
@@ -0,0 +1,8 @@
+# include <stdio.h>
+void main()
+{
+    int n;
+    printf("enter a natural number:");
+    scanf("%d",&n);
+    printf("sum of natral number upto a natual number %d is , n, (n*(n+1))");
+}
